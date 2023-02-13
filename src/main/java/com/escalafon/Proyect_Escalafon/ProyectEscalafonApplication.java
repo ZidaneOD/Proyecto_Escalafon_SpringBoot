23e2a77b643554bb2fdfe589ae573a386d75d708
Zidane Ortiz <zidaneortiz16@gmail.com>
2023-02-12 20:36:24 -0500

    Primer Commit

---
+++ src/main/java/com/escalafon/Proyect_Escalafon/ProyectEscalafonApplication.java
@@ -0,0 +1,13 @@
+package com.escalafon.Proyect_Escalafon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectEscalafonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectEscalafonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escalafon/Proyect_Escalafon/ProyectEscalafonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escalafon.Proyect_Escalafon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectEscalafonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
